c350422591047ad97afb5f7b5f15a984ceae862e
Vasyl Nakvasiuk <vaxxxa@gmail.com>
2014-06-05 23:48:09 +0300

    local oh-my-zsh.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,13 +3,13 @@
 cd "$(dirname "$0")"
 
 function doIt() {
-    rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.rst" -av . ~
+    rsync --exclude ".git/" --exclude ".DS_Store" --exclude "Makefile" --exclude "bootstrap.sh" --exclude "README.rst" -av . ~
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
+if [ "$1" = "--force" -o "$1" = "-f" ]; then
     doIt
 else
-    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+    vared -p "This may overwrite existing files in your home directory. Are you sure? (y/n)" -c REPLY
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]; then
         doIt
