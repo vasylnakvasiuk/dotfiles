1fe9b97182c060b8ccff7ca423dfc711d452cf1c
Vasyl Nakvasiuk <vaxxxa@gmail.com>
2014-06-14 00:15:55 +0300

    add new battery script, show "swap" info

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-BREW_PACKAGES=(git mc htop iftop ifstat wget mtr tree cloc tmux tmux-mem-cpu-load cmatrix figlet)
+BREW_PACKAGES=(git mc htop iftop ifstat wget mtr tree ranger cloc tmux tmux-mem-cpu-load spark cmatrix figlet)
 
 function install_brew_packages() {
     for index in $BREW_PACKAGES; do
