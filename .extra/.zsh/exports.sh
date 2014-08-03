6fd33847c1d8a23bc9c95f21d330a1cb8ab2b0bc
Vasyl Nakvasiuk <vaxxxa@gmail.com>
2014-08-03 11:58:15 +0300

    add base16-shell script

--- .extra/.zsh/exports.sh
+++ .extra/.zsh/exports.sh
@@ -13,3 +13,8 @@ export LANG=en_US.UTF-8
 
 # PATH environment variable
 export PATH="/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:$HOME/.extra/bin"
+
+# Base16 Shell
+BASE16_SCHEME="railscasts"
+BASE16_SHELL="$HOME/.extra/.zsh/base16-shell/base16-$BASE16_SCHEME.dark.sh"
+[[ -s $BASE16_SHELL ]] && . $BASE16_SHELL
