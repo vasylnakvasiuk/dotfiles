fe79fdadee7c5e1104354e78ceb8d2a25c142be9
Vasyl Nakvasiuk <vaxxxa@gmail.com>
2015-04-19 19:27:27 +0300

    arp packages

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-BREW_PACKAGES=(zsh git mc vim htop iftop ifstat nmap wget mtr wrk tree ag cloc task vit tmux tmux-mem-cpu-load reattach-to-user-namespace spark cmatrix figlet)
+BREW_PACKAGES=(zsh git mc vim htop iftop ifstat nmap arp-scan arp-sk arpoison wget mtr wrk tree ag cloc tmux tmux-mem-cpu-load reattach-to-user-namespace spark cmatrix figlet)
 
 function install_brew_packages() {
     for index in $BREW_PACKAGES; do
