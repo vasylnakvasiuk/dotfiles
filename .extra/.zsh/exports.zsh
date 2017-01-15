cd3fe715a949426e2af3b77161b766e417a1ad2b
Vasyl Nakvasiuk <vaxxxa@gmail.com>
2017-01-15 20:12:12 +0200

    fix C-H in terminal again

--- .extra/.zsh/exports.zsh
+++ .extra/.zsh/exports.zsh
@@ -24,4 +24,7 @@ export FZF_DEFAULT_COMMAND='ag -g ""'
 export FZF_DEFAULT_OPTS='--bind=ctrl-z:toggle-up'
 
 # With that fix <C-H> works in neovim
+# https://github.com/neovim/neovim/issues/2048#issuecomment-78045837
 export TERMINFO="$HOME/.terminfo"
+[[ -f ~/.$TERM.ti ]] || infocmp $TERM | sed 's/kbs=^[hH]/kbs=\\177/' > ~/.$TERM.ti
+tic ~/.$TERM.ti
