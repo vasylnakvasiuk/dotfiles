d5bab054d188b55512b9e7282ced0307a4859474
Vasyl Nakvasiuk <vaxxxa@gmail.com>
2018-09-08 18:00:07 +0300

    Use fzf-down as alias

--- .extra/.zsh/fzf/extra.zsh
+++ .extra/.zsh/fzf/extra.zsh
@@ -4,9 +4,7 @@ is_in_git_repo() {
   git rev-parse HEAD > /dev/null 2>&1
 }
 
-fzf-down() {
-  fzf --height 50% "$@" --border
-}
+alias fzf-down="fzf --height 50% --border"
 
 gf() {
   is_in_git_repo || return
