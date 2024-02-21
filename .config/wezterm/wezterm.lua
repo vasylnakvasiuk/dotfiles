55b4cfee718d95a729d30e33a9e55c281ad4e145
Vasyl Nakvasiuk <vaxxxa@gmail.com>
2024-02-22 00:19:40 +0200

    set OneDark for wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,6 +5,7 @@ return {
     family = 'FiraCode Nerd Font',
     harfbuzz_features = { 'ss05' },
   },
+  color_scheme = "OneDark (base16)",
   hide_tab_bar_if_only_one_tab = true,
   cell_width = 0.9,
 }
