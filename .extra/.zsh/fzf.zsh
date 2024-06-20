d48c32889910088f08b9073cfb3a2ed26a54418c
Vasyl Nakvasiuk <vaxxxa@gmail.com>
2024-06-20 14:43:45 +0300

    zsh: remove old fzf git commands

--- .extra/.zsh/fzf.zsh
+++ .extra/.zsh/fzf.zsh
@@ -6,46 +6,6 @@ is_in_git_repo() {
 
 alias fzf-down="fzf --height 50% --border"
 
-gf() {
-  is_in_git_repo || return
-  git -c color.status=always status --short |
-  fzf-down -m --ansi --nth 2..,.. \
-    --preview '(git diff --color=always -- {-1} | sed 1,4d; cat {-1}) | head -500' |
-  cut -c4- | sed 's/.* -> //'
-}
-
-gb() {
-  is_in_git_repo || return
-  git branch -a --color=always | grep -v '/HEAD' | sort |
-  fzf-down --ansi --multi --tac --preview-window right:70% \
-    --preview 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" $(sed s/^..// <<< {} | cut -d" " -f1) | head -'$LINES |
-  sed 's/^..//' | cut -d' ' -f1 | sed 's#^remotes/##'
-}
-
-gt() {
-  is_in_git_repo || return
-  git tag --sort -version:refname |
-  fzf-down --multi --preview-window right:70% \
-    --preview 'git show --color=always {} | head -'$LINES
-}
-
-gh() {
-  is_in_git_repo || return
-  git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph --color=always |
-  fzf-down --ansi --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
-    --header 'Press CTRL-S to toggle sort' \
-    --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always | head -'$LINES |
-  grep -o "[a-f0-9]\{7,\}"
-}
-
-gr() {
-  is_in_git_repo || return
-  git remote -v | awk '{print $1 "\t" $2}' | uniq |
-  fzf-down --tac \
-    --preview 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" {1} | head -200' |
-  cut -d$'\t' -f1
-}
-
 # Checkout git commit
 fzf-checkout-commit() {
   is_in_git_repo || return
@@ -56,6 +16,7 @@ fzf-checkout-commit() {
   grep -o "[a-f0-9]\{7,\}" | { read commit; git checkout $commit }
 }
 bindkey -s '^Go' 'fzf-checkout-commit\n'
+bindkey -s '^G^O' 'fzf-checkout-commit\n'
 
 fzf-checkout-branch() {
   is_in_git_repo || return
@@ -65,8 +26,8 @@ fzf-checkout-branch() {
   sed "s/.* //" | sed "s#remotes/[^/]*/##" | { read branch; git checkout $branch }
 }
 bindkey -s '^Gk' 'fzf-checkout-branch\n'
+bindkey -s '^G^K' 'fzf-checkout-branch\n'
 
-# Git log
 fzf-log() {
   is_in_git_repo || return
   git log --graph --color=always \
@@ -80,26 +41,8 @@ fzf-log() {
   zle redisplay
 }
 zle -N fzf-log
-bindkey '^Gl' fzf-log
-
-
-join-lines() {
-  local item
-  while read item; do
-    echo -n "${(q)item} "
-  done
-}
-
-bind-git-helper() {
-  local c
-  for c in $@; do
-    eval "fzf-g$c-widget() { local result=\$(g$c | join-lines); zle reset-prompt; LBUFFER+=\$result }"
-    eval "zle -N fzf-g$c-widget"
-    eval "bindkey '^G$c' fzf-g$c-widget"
-  done
-}
-bind-git-helper f b t h r
-unset -f bind-git-helper
+bindkey '^Gj' fzf-log
+bindkey '^G^J' fzf-log
 
 fzf-flog() {
   # Usage:
@@ -124,24 +67,11 @@ fzf-flog() {
   FZF-EOF"
   zle redisplay
 }
+zle -N fzf-flog
+bindkey '^Gu' fzf-flog
+bindkey '^G^U' fzf-flog
 
 # -------------------------------- Git bindings --------------------------------
-
-
-# -------------------------------- Quick bindings --------------------------------
-
-quick-docker-containers() {
-  docker ps -a --format "table {{.Names}}\t{{.ID}}\t{{.Status}}" | tail -n +2 |
-  fzf-down --ansi --multi --tac --preview-window right:60% \
-    --preview 'docker top {1} | head -'$LINES |
-  cut -d' ' -f1
-}
-
-quick-gd-widget() { local result=$(quick-docker-containers | join-lines); zle reset-prompt; LBUFFER+=$result }
-zle -N quick-gd-widget
-bindkey '^Qd' quick-gd-widget
-
-# -------------------------------- Quick bindings --------------------------------
 
 diskripper() {
   local VOLUME FILENAME
