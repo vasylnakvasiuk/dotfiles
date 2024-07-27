992a1f354769a0faade03d2e0337f8732349615c
Vasyl Nakvasiuk <vaxxxa@gmail.com>
2024-07-27 20:57:35 +0300

    zsh: powerup history

--- .extra/.zsh/exports.zsh
+++ .extra/.zsh/exports.zsh
@@ -17,6 +17,21 @@ export PATH="/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/h
 # Disable auto renaming window title in tmux
 DISABLE_AUTO_TITLE=true
 
+# Number of commands to keep in the history list (in memory)
+HISTSIZE=1000000
+
+# Number of commands to save in the history file
+SAVEHIST=$HISTSIZE
+
+# Ignore commands that start with a space
+setopt hist_ignore_space
+
+# Append history incrementally as each command is executed
+setopt inc_append_history
+
+# Share history across all sessions
+setopt share_history
+
 # Now fzf uses ag like a backend
 export FZF_DEFAULT_COMMAND='rg --files --sort-files --hidden --follow --glob "!.git"'
 export FZF_DEFAULT_OPTS='--color=dark,bg+:18 --bind=ctrl-z:toggle-up --inline-info --height 50% --no-reverse --border'
